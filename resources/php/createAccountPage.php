4f6bf6390933112878b88b3ee891082e315dd8a5
gszp1 <grzybekmateusz@poczta.fm>
2023-12-14 23:04:58 +0100

    Fixed labels in createAccountPage.php

--- resources/php/createAccountPage.php
+++ resources/php/createAccountPage.php
@@ -1,8 +1,8 @@
 <?php include ("server.php"); ?>
 
 <!DOCTYPE html>
-<html>
-    <head lang="en">
+<html lang="en">
+    <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Create Account</title>
@@ -14,13 +14,13 @@
                 Create account
             </div>
             <form class="formCAP" action="createAccountPage.php" method="post">
-                <label for="name">Username:</label><br>
+                <label>Username:</label><br>
                 <input class="formInputCAP" type="text" id="uname" name="username"><br>
-                <label for="userEmail">E-mail:</label><br>
+                <label>E-mail:</label><br>
                 <input class="formInputCAP" type="text" id="email" name="userEmail"><br>
-                <label for="password">Password:</label><br>
+                <label>Password:</label><br>
                 <input class="formInputCAP" type="password" id="password" name="password"><br>
-                <label for="confirm password">Password:</label><br>
+                <label>Confirm password:</label><br>
                 <input class="formInputCAP" type="password" id="passwordConfirm" name="passwordConfirmation"><br>
             </form>
             <div id="buttonsContainerCAP">
