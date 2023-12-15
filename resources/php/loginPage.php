726ab2d7791feb1af97fd699925fd0f3a43019c2
gszp1 <grzybekmateusz@poczta.fm>
2023-12-15 10:55:54 +0100

    Moved lang=en to <html> in each file containg html

--- resources/php/loginPage.php
+++ resources/php/loginPage.php
@@ -2,8 +2,8 @@
     include("server.php");
 ?>
 <!DOCTYPE html>
-<html>
-    <head lang="en">
+<html lang="en">
+    <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login page</title>
