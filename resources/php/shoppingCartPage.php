27e1a4bfe7c73c62ce3188df38755d37598c6ef6
gszp1 <grzybekmateusz@poczta.fm>
2024-01-02 00:25:20 +0100

    Adds head and body with navbar to shoppingCartPage.php

--- resources/php/shoppingCartPage.php
+++ resources/php/shoppingCartPage.php
@@ -1,1 +1,23 @@
 <!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Create Account</title>
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    <body>
+        <nav class="navbar" id="navbar">
+            <div id="shopLogoNavbarI">Part Shop</div>
+            <div id="searchbarNavbarWrapperI">
+                <form id="searchBarI">
+                    <input id="searchBarInputI" type="text" id="productName" name=productName><br>
+                </form>
+                <button id="searchButtonI">Search</button>
+            </div>
+            <button onclick="goToPage('./resources/php/userAccountPage.php')" id="loginButtonI">Your account</button>
+            <button onclick="goToPage('./resources/php/shoppingCartPage.php')" id="shoppingCartI">Shopping cart</button>
+        </nav>
+    </body>
+</html>
