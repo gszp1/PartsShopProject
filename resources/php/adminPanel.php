3a587b1d54793c8e7a3d8794148ac9f0b02eb5b3
gszp1 <grzybekmateusz@poczta.fm>
2024-01-12 11:40:42 +0100

    Adds adminPanel contents.

---
+++ resources/php/adminPanel.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Create Account</title>
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    <body>
+        <nav class="navbarNew">
+            <div id="shopLogoNavbarI">Part Shop</div>
+            <div id="searchbarNavbarWrapperI">
+                <form id="searchBarI">
+                    <input id="searchBarInputI" type="text" id="productName" name=productName><br>
+                </form>
+                <button id="searchButtonI">Search</button>
+            </div>
+            <button onclick="goToPage('./resources/php/userAccountPage.php')" id="loginButtonI">Your account</button>
+            <button onclick="goToPage('./resources/php/shoppingCartPage.php')" id="shoppingCartI">Shopping cart</button>
+        </nav>
+        <div class="pageContents">
+            
+        </div>
+    </body>
+</html>
