a6e745ce3b9663d67dccfc529d8ba6bf82109ba3
gszp1 <grzybekmateusz@poczta.fm>
2024-01-19 11:23:33 +0100

    Changes naming convention to camel-case in server.php.

--- resources/php/server.php
+++ resources/php/server.php
@@ -4,7 +4,6 @@
     $username = "";
     $email = "";
     $errors = array();
-    // establish database connection
     $dbConnection = mysqli_connect('localhost', 'dbclient', 'ar0220', 'partshopdb');
 
     if ($dbConnection == false) {
@@ -16,7 +15,7 @@
         $email = mysqli_real_escape_string($dbConnection, $_POST['userEmail']);
         $password = mysqli_real_escape_string($dbConnection, $_POST["password"]);
         $passwordConfirmation = mysqli_real_escape_string($dbConnection, $_POST["passwordConfirmation"]);
-        
+
         if (empty($username) || empty($email) || empty($password) || empty($passwordConfirmation)) {
             array_push($errors,"All field have to be filled.");
         } else {
@@ -25,7 +24,7 @@
             }
             if ($password != $passwordConfirmation) {
                 array_push($errors,"Provided passwords don't match.");
-            }    
+            }
         }
 
         $getUserFromDBQuery = "SELECT * FROM customers WHERE Username='$username' OR Email='$email' LIMIT 1";
@@ -42,10 +41,10 @@
         }
             // register user
         if (count($errors) == 0) {
-            $password_escp = md5($password);
-            $prepared_statement = $dbConnection->prepare("INSERT INTO customers(Email, Username, Password) VALUES (?, ?, ?)");
-            $prepared_statement->bind_param("sss", $email, $username, $password_escp);
-            $prepared_statement->execute();
+            $passwordHash = md5($password);
+            $preparedStatement = $dbConnection->prepare("INSERT INTO customers(Email, Username, Password) VALUES (?, ?, ?)");
+            $preparedStatement->bind_param("sss", $email, $username, $passwordHash);
+            $preparedStatement->execute();
             header('location: loginPage.php');
             exit();
         }
@@ -54,7 +53,7 @@
     if(isset($_POST['loginUser'])) {
         $email = mysqli_real_escape_string($dbConnection, $_POST['email']);
         $password = mysqli_real_escape_string($dbConnection, $_POST['password']);
-        
+
         if (empty($email) || empty($password)) {
             array_push($errors, "All fields have to be filled.");
         } elseif(validate_email($email) == false) {
@@ -62,10 +61,10 @@
         }
 
         if (count($errors) == 0) {
-            $password_hash = md5($password);
-            $query = "SELECT * FROM customers WHERE Email='$email' AND Password='$password_hash'";
+            $passwordHash = md5($password);
+            $query = "SELECT * FROM customers WHERE Email='$email' AND Password='$passwordHash'";
             $results = mysqli_query($dbConnection, $query);
-            
+
             if (mysqli_num_rows($results) == 1) {
                 $_SESSION['userID'] = mysqli_fetch_assoc($results)['CustomerID'];
                 $gotoPage = '/../index.php';
@@ -75,14 +74,14 @@
                 header("Location: ". $gotoPage);
                 exit();
             } else {
-                $query = "SELECT * FROM admin WHERE email='$email' AND password='$password_hash'";
+                $query = "SELECT * FROM admin WHERE email='$email' AND password='$passwordHash'";
                 $results = mysqli_query($dbConnection, $query);
                 if (mysqli_num_rows($results) == 1) {
                     header("Location: adminPanel.php");
                     exit();
                 } else {
                     array_push($errors,"Provided credentials are incorrect.");
-                }                
+                }
             }
         }
     }
