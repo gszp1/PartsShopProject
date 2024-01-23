0f0234d1a5e23ea2dd8919c42e30846c9c594288
gszp1 <grzybekmateusz@poczta.fm>
2024-01-23 23:35:13 +0100

    Moved script header to end of body.

--- resources/php/pages/shoppingCartPage.php
+++ resources/php/pages/shoppingCartPage.php
@@ -16,7 +16,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Create Account</title>
         <link rel="stylesheet" href="./../../css/styles.css">
-        <script src="./../../js/script.js"></script>
     </head>
     <body>
         <nav class="navbarNew">
@@ -38,5 +37,6 @@
                 ?>
             </div>
         </div>
+        <script src="./../../js/script.js"></script>
     </body>
 </html>
