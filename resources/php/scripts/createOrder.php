e7383edd3929ea65f5bc5096c1dfd00dc8fd5d2b
gszp1 <grzybekmateusz@poczta.fm>
2024-01-24 18:42:39 +0100

    Adds creating new order.

--- resources/php/scripts/createOrder.php
+++ resources/php/scripts/createOrder.php
@@ -2,3 +2,24 @@
 
     include('functions.php');
 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $dbConnection = connect_with_database();
+        if ($dbConnection === null) {
+            exit("Failed to connect with database.");
+        }
+
+        // query for adding order.
+        $query = "INSERT INTO orders(CustomerID, Status) VALUES (?, 0)";
+        $preparedStatement = $dbConnection->prepare($query);
+        if ($preparedStatement === false) {
+            exit("Failed to prepare statement.");
+        }
+        $userID = mysqli_real_escape_string($dbConnection, $_POST['customerID']);
+        $preparedStatement->bind_param("i", $userID);
+        if (!$preparedStatement->execute()) {
+            exit("Execution failed: " . $preparedStatement->error);
+        }
+
+        $preparedStatement->close();
+        $dbConnection->close();
+    }
