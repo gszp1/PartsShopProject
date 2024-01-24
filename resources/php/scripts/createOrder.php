a4c16e66501e366790099c834054cb061dabd4e5
gszp1 <grzybekmateusz@poczta.fm>
2024-01-24 19:06:16 +0100

    Adds retrieving products from shoppingCart.

--- resources/php/scripts/createOrder.php
+++ resources/php/scripts/createOrder.php
@@ -8,13 +8,28 @@
             exit("Failed to connect with database.");
         }
 
+        // Retrieve products from shopping cart
+        $query = "SELECT ProductID, Quantity, Price FROM shoppingcart WHERE CustomerID = ?";
+        $preparedStatement = $dbConnection->prepare($query);
+        if ($preparedStatement === false) {
+            exit("Failed to prepare statement.");
+        }
+        $userID = mysqli_real_escape_string($dbConnection, $_POST['customerID']);
+        $preparedStatement->bind_param("i", $userID);
+        if (!$preparedStatement->execute()) {
+            exit("Execution failed: " . $preparedStatement->error);
+        }
+        $cartProducts = $preparedStatement->get_result();
+        if ($result->num_rows <= 0) {
+            exit("No records to add.");
+        }
+
         // query for adding order.
         $query = "INSERT INTO orders(CustomerID, Status) VALUES (?, 0)";
         $preparedStatement = $dbConnection->prepare($query);
         if ($preparedStatement === false) {
             exit("Failed to prepare statement.");
         }
-        $userID = mysqli_real_escape_string($dbConnection, $_POST['customerID']);
         $preparedStatement->bind_param("i", $userID);
         if (!$preparedStatement->execute()) {
             exit("Execution failed: " . $preparedStatement->error);
@@ -31,12 +46,15 @@
             exit("Execution failed: " . $preparedStatement->error);
         }
         $result = $preparedStatement->get_result();
+        if ($result === false) {
+            exit("Error fetching result: " . $preparedStatement->error);
+        }
         $row = $result->fetch_assoc();
         if ($row === null) {
             exit("Order was not created.");
         }
         $OrderID = $row['newOrderID'];
-        
+
 
 
 
