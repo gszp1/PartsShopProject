e2e864fc07b21e32c53cf283a807d2fe02b46e49
gszp1 <grzybekmateusz@poczta.fm>
2024-01-24 18:56:22 +0100

    Adds retrieving newOrderID from DB.

--- resources/php/scripts/createOrder.php
+++ resources/php/scripts/createOrder.php
@@ -20,6 +20,23 @@
             exit("Execution failed: " . $preparedStatement->error);
         }
 
+        // Retrieve new order's ID
+        $query = "SELECT MAX(OrderID) AS newOrderID FROM orders WHERE CustomerID = ?";
+        $preparedStatement = $dbConnection->prepare($query);
+        if ($preparedStatement === false) {
+            exit("Failed to prepare statement.");
+        }
+        $preparedStatement->bind_param("i", $userID);
+        if (!$preparedStatement->execute()) {
+            exit("Execution failed: " . $preparedStatement->error);
+        }
+        $result = $preparedStatement->get_result();
+        $row = $result->fetch_assoc();
+        $OrderID = $row['newOrderID'];
+        
+
+
+
         $preparedStatement->close();
         $dbConnection->close();
     }
