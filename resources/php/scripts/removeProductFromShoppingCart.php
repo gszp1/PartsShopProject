dcf6190ad84aefdf8634d99e83f7182716dac544
gszp1 <grzybekmateusz@poczta.fm>
2024-01-24 11:07:00 +0100

    Adds script body to removeProductFromShoppingCart.php.

--- resources/php/scripts/removeProductFromShoppingCart.php
+++ resources/php/scripts/removeProductFromShoppingCart.php
@@ -8,8 +8,20 @@
             exit("Failed to connect with database.");
         }
 
-        $query = "DELETE * FROM shoppincart WHERE ProductID=?, CustomerID=?";
+        $query = "DELETE * FROM shoppincart WHERE ProductID=? AND CustomerID=?";
         $userID = mysqli_real_escape_string($dbConnection, $_POST['userID']);
         $productID = mysqli_real_escape_string($dbConnection, $_POST['$productID']);
+        $preparedStatement = $dbConnection->prepare($query);
+        if ($preparedStatement === false) {
+            exit("Failed to prepare statement.");
+        }
+        $preparedStatement->bind_param("ii", $productID, $userID);
+        if (!$preparedStatement->execute()) {
+            exit("Execution failed: " . $preparedStatement->error);
+        }
+        $preparedStatement->close();
 
+        // Close the database connection
+        $dbConnection->close();
+        echo "Success.";
     }
