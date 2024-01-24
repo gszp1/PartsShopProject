3fd3b181f1d3777bda742ad3c4e3850912b461f3
gszp1 <grzybekmateusz@poczta.fm>
2024-01-24 11:54:08 +0100

    Changes.

--- resources/php/pages/shoppingCartPage.php
+++ resources/php/pages/shoppingCartPage.php
@@ -16,7 +16,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Create Account</title>
         <link rel="stylesheet" href="./../../css/styles.css">
-        <script src="./../../js/script.js"></script>
+        <script type="text/javascript" src="./../../js/shoppingCartScript.js"></script>
     </head>
     <body>
         <nav class="navbarNew">
