a735f6299082f2253b548c6b2dfabc2408c8970b
gszp1 <grzybekmateusz@poczta.fm>
2024-01-25 08:29:28 +0100

    Adds displaying product data.

--- resources/php/scripts/getProductInfo.php
+++ resources/php/scripts/getProductInfo.php
@@ -29,8 +29,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $Picture = $row['Picture'];
         $Discontinued = $row['Discontinued'];
 
-        $productInfo = "<tr><th>Product Name</th><th>Price</th><th>Units In Stock</th></tr>";
-        $productInfo .= "<tr><td>$ProductName</td><td>$Price</td><td>$UnitsInStock</td></tr>";
+        $productInfo =  "<tr><td>CategoryID</td><td>$CategoryID</td></tr>";
+        $productInfo .= "<tr><td>SupplierID</td><td>$SupplierID</td></tr>";
+        $productInfo .= "<tr><td>ManufacturerID</td><td>$ManufacturerID</td></tr>";
+        $productInfo .= "<tr><td>ProductName</td><td>$ProductName</td></tr>";
+        $productInfo .= "<tr><td>Price</td><td>$Price</td></tr>";
+        $productInfo .= "<tr><td>UnitsInStock</td><td>$UnitsInStock</td></tr>";
+        $productInfo .= "<tr><td>OrderedUnits</td><td>$OrderedUnits</td></tr>";
+        $productInfo .= "<tr><td>Picture</td><td>$Picture</td></tr>";
+        $productInfo .= "<tr><td>Discontinued</td><td>$Discontinued</td></tr>";
         echo $productInfo;
     }
     $preparedStatement->close();
