cfdfecc596b2f2803b9e5bdddca69ae61d5c349c
gszp1 <grzybekmateusz@poczta.fm>
2024-01-25 01:33:21 +0100

    Fixes wrong param type.

--- resources/php/scripts/updateOrderStatus.php
+++ resources/php/scripts/updateOrderStatus.php
@@ -19,7 +19,7 @@
 
         $orderID = mysqli_real_escape_string($dbConnection, $_POST['orderID']);
         $status = mysqli_real_escape_string($dbConnection, $_POST['status']);
-        $preparedStatement->bind_param("ss", $status, $orderID);
+        $preparedStatement->bind_param("ii", $status, $orderID);
         $preparedStatement->execute();
         if ($preparedStatement->error) {
             exit("Execution error: " . $preparedStatement->error);
