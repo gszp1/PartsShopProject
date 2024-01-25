c846676f87f4c30d11b54668027b8110ff214275
gszp1 <grzybekmateusz@poczta.fm>
2024-01-25 03:29:49 +0100

    Adds functionality to loadProductsNames.php

--- resources/php/scripts/loadProductsNames.php
+++ resources/php/scripts/loadProductsNames.php
@@ -1,3 +1,18 @@
 <?php
 
-    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $dbConnection = connect_with_database();
+        if ($dbConnection === null) {
+            exit("Failed to connect with database.");
+        }
+
+        $query = "SELECT ProductID, ProductName FROM products";
+        $productNames = mysqli_query($dbConnection, $query);
+
+        if ($productNames->num_rows > 0) {
+            while ($row = $productNames->fetch_assoc()) {
+                echo '<option value="' . $row['ProductID'] . '">' . $row['ProductName'] . '</option>';
+            }
+        }
+        mysqli_close($dbConnection);
+    }
