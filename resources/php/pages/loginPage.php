2edcb2170b071cc64ced8419a56d70195ff5cf90
gszp1 <grzybekmateusz@poczta.fm>
2024-01-26 04:20:42 +0100

    Adds navbar.

--- resources/php/pages/loginPage.php
+++ resources/php/pages/loginPage.php
@@ -10,21 +10,29 @@
         <link rel="stylesheet" href="/resources/css/styles.css">
         <script src="./../../js/script.js"></script>
     </head>
-    <body style="justify-content: center;">
-        <div id="loginBoxLP">
-            <div id="greetingLP">
-                Login
-            </div>
-            <form method="post" class="formLP" action="loginPage.php">
-                <label for="userEmail">E-mail:</label><br>
-                <input class="formInputLP" type="text" id="userEmail" name=email><br>
-                <label for="userPassword">Password:</label><br>
-                <input class="formInputLP" type="password" id="userPassword" name=password><br>
-                <div id="buttonsBoxLP">
-                    <button type="submit" id="loginButtonLP" name="loginUser">Login</button>
-                    <button type="button" onclick="goToPage('createAccountPage.php')" id="registerButtonLP">Register</button>
+    <body>
+        <nav class="navbarNew">
+            <a id="shopLogoNavbarI" href="./../../../index.php">Part Shop</a>
+            <div id="searchbarNavbarWrapperI"></div>
+            <button onclick="goToPage('userAccountPage.php')" id="loginButtonI">Your account</button>
+            <button onclick="goToPage('shoppingCartPage.php')" id="shoppingCartI">Shopping cart</button>
+        </nav>
+        <div class="pageContents">
+            <div id="loginBoxLP">
+                <div id="greetingLP">
+                    Login
                 </div>
-            </form>
+                <form method="post" class="formLP" action="loginPage.php">
+                    <label for="userEmail">E-mail:</label><br>
+                    <input class="formInputLP" type="text" id="userEmail" name=email><br>
+                    <label for="userPassword">Password:</label><br>
+                    <input class="formInputLP" type="password" id="userPassword" name=password><br>
+                    <div id="buttonsBoxLP">
+                        <button type="submit" id="loginButtonLP" name="loginUser">Login</button>
+                        <button type="button" onclick="goToPage('createAccountPage.php')" id="registerButtonLP">Register</button>
+                    </div>
+                </form>
+            </div>
         </div>
         <?php
             include("./../misc/errors.php");
