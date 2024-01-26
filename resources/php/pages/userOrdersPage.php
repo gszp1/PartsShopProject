1ab36235cf24f54335f40f25193983569913c288
gszp1 <grzybekmateusz@poczta.fm>
2024-01-26 01:26:27 +0100

    Changes page name to Order details.

--- resources/php/pages/userOrdersPage.php
+++ resources/php/pages/userOrdersPage.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Admin Panel</title>
+        <title>Order details</title>
         <link rel="stylesheet" href="./../../css/styles.css">
         <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
         <script src="./../../js/script.js"></script>
