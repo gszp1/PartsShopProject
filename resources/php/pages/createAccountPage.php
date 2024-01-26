b774320b0f982d38b2f3cec317cd5e7b0604987d
gszp1 <grzybekmateusz@poczta.fm>
2024-01-26 03:27:40 +0100

    Adds navbar to createAccountPage.php.

--- resources/php/pages/createAccountPage.php
+++ resources/php/pages/createAccountPage.php
@@ -11,23 +11,31 @@
         <link rel="stylesheet" href="./../../css/styles.css">
     </head>
     <body>
-        <div id="accountCreationBoxCAP">
-            <div id="greetingCAP">
-                Create account
-            </div>
-            <form class="formCAP" action="createAccountPage.php" method="post">
-                <label>Username:</label><br>
-                <input class="formInputCAP" type="text" id="uname" name="username"><br>
-                <label>E-mail:</label><br>
-                <input class="formInputCAP" type="text" id="email" name="userEmail"><br>
-                <label>Password:</label><br>
-                <input class="formInputCAP" type="password" id="password" name="password"><br>
-                <label>Confirm password:</label><br>
-                <input class="formInputCAP" type="password" id="passwordConfirm" name="passwordConfirmation"><br>
-                <div id="buttonsContainerCAP">
-                    <button type="submit" id="createAccountButtonCAP" name="registerUser">Create account</button>
+        <nav class="navbarNew">
+            <a id="shopLogoNavbarI" href="./../../../index.php">Part Shop</a>
+            <div id="searchbarNavbarWrapperI"></div>
+            <button onclick="goToPage('userAccountPage.php')" id="loginButtonI">Your account</button>
+            <button onclick="goToPage('shoppingCartPage.php')" id="shoppingCartI">Shopping cart</button>
+        </nav>
+        <div class="pageContents">
+            <div id="accountCreationBoxCAP">
+                <div id="greetingCAP">
+                    Create account
                 </div>
-            </form>
+                <form class="formCAP" action="createAccountPage.php" method="post">
+                    <label>Username:</label><br>
+                    <input class="formInputCAP" type="text" id="uname" name="username"><br>
+                    <label>E-mail:</label><br>
+                    <input class="formInputCAP" type="text" id="email" name="userEmail"><br>
+                    <label>Password:</label><br>
+                    <input class="formInputCAP" type="password" id="password" name="password"><br>
+                    <label>Confirm password:</label><br>
+                    <input class="formInputCAP" type="password" id="passwordConfirm" name="passwordConfirmation"><br>
+                    <div id="buttonsContainerCAP">
+                        <button type="submit" id="createAccountButtonCAP" name="registerUser">Create account</button>
+                    </div>
+                </form>
+            </div>
         </div>
         <?php
             include('./../misc/errors.php');
